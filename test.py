774050285c6ac2cbae3c16b0285d4d46a0326970
Kamata Noriyuki <kamanori3@gmail.com>
2014-06-01 20:12:06 +0900

    コメントの追記

--- test.py
+++ test.py
@@ -1,17 +1,19 @@
+# -*- coding: utf-8 -*-  ～  "".join(row)) までを張り付ける。(^o^)。
+
+
 # -*- coding: utf-8 -*-
 
 import csv   #csvモジュールをインポートする
-import os
-import shutil
+import os    #osモジュールをインポートする
+import shutil shutilモジュールをインポートする。
 
-f = open('C:\\Users\\kaman_000\\Desktop\\list.csv','r', encoding='utf-8')
+f = open('C:\\Users\\kaman_000\\Desktop\\list.csv','r', encoding='utf-8') #★★CSVファイルの置き場
 
 dataReader = csv.reader(f)
 
-s_dir="d:\\Users\\kaman_000\\Documents\\80 書籍\\evernote移動済み\\"
-t_dir="C:\\Users\\kaman_000\\Desktop\\test\\"
+s_dir="d:\\Users\\kaman_000\\Documents\\80 書籍\\evernote移動済み\\" #★★PDFファイルの置き場所
+t_dir="C:\\Users\\kaman_000\\Desktop\\test\\" #★★コピー先
 
 for row in dataReader:
     print(row)
     shutil.copy(s_dir + "".join(row),t_dir + "".join(row))
-
